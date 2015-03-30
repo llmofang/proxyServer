a98ed9b16dbf738aa40a06c6689d31d6c5765295
wengcan <572037922@qq.com>
2015-03-30 16:08:21 +0800

    修改：     build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 export GOPATH=`pwd`
-if [ -e  `pwd`+"/src/github.com/astaxie/redis" ]
+if [ ! -d  `pwd`"/src/github.com/hoisie/redis" ]
 then	
-	echo 'getting github.com/hoisie/redis'
+	echo '==> Getting dependencies (hoisie/redis) ...'
 	go get  github.com/hoisie/redis
 fi
-if [ -e  `pwd`+"/src/github.com/op/go-logging" ]
+if [ ! -d  `pwd`"/src/github.com/op/go-logging" ]
 then	
-	echo 'getting github.com/op/go-logging'
+	echo '==> Getting dependencies (op/go-logging) ...'
 	go get  github.com/op/go-logging
 fi
+echo '==> building'
 go build main && mv main ./bin
