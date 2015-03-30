4cb30f13ba04257cf9c6565c7d01119972cd785e
wengcan <572037922@qq.com>
2015-03-30 15:37:09 +0800

    修改：     build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 export GOPATH=`pwd`
+if [ -e  `pwd`+"/src/github.com/astaxie/redis" ]
+then	
+	echo 'getting github.com/hoisie/redis'
+	go get  github.com/hoisie/redis
+fi
+if [ -e  `pwd`+"/src/github.com/op/go-logging" ]
+then	
+	echo 'getting github.com/op/go-logging'
+	go get  github.com/op/go-logging
+fi
 go build main && mv main ./bin
